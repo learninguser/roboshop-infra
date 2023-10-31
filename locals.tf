65cb421c15f978f1a89c29772c321b0c5e4d29d7
Pavan Kumar Akula <akulapavankumar@outlook.com>
2023-10-31 20:36:59 +0100

    Print private and database subnet ids to console

--- locals.tf
+++ locals.tf
@@ -2,4 +2,6 @@ locals {
   vpc_id = module.this.vpc_id
   azs = module.this.azs
   public_subnet_ids = module.this.public_subnet_ids
+  private_subnet_ids = module.this.private_subnet_ids
+  database_subnet_ids = module.this.database_subnet_ids
 }
