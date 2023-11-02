7e021782f0d156094e52c71a99581c0c1debb7f3
Pavan Kumar Akula <akulapavankumar@outlook.com>
2023-11-02 21:30:06 +0100

    Add shell script for setting up roboshop project

--- roboshop-ansible.sh
+++ roboshop-ansible.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 yum install ansible -y
+cd /tmp
+git clone https://github.com/learninguser/jd-roboshop-ansible.git
+
+cd jd-roboshop-ansible
+
+for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis dispatch; do
+    ansible-playbook -i inventory -e ansible-user=centos -e ansible-passwod=DevOps321 "${component}.yaml"
+done
