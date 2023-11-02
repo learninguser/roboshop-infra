d25b583afe642cb39f3a06a0ac9b57f4366b81ac
Pavan Kumar Akula <akulapavankumar@outlook.com>
2023-11-02 22:19:26 +0100

    Correct typo

--- roboshop-ansible.sh
+++ roboshop-ansible.sh
@@ -6,6 +6,6 @@ git clone https://github.com/learninguser/jd-roboshop-ansible.git
 
 cd jd-roboshop-ansible
 
-for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis dispatch; do
-    ansible-playbook -i inventory -e ansible-user=centos -e ansible-passwod=DevOps321 "${component}.yaml"
+for component in frontend mongodb catalogue redis user cart mysql shipping rabbitmq payment dispatch; do
+    ansible-playbook -i inventory -e ansible_user=centos -e ansible_passwod=DevOps321 "${component}.yaml"
 done
