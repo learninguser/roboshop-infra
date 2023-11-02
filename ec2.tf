9eea0ecfa146b9e165196c23c23ad7f039b75abf
Pavan Kumar Akula <akulapavankumar@outlook.com>
2023-11-02 21:11:46 +0100

    Add script for creating EC2, installing Ansible with user_data

--- ec2.tf
+++ ec2.tf
@@ -13,7 +13,24 @@ module "ec2_instance" {
 
   tags = merge(
     {
-        Name = each.key
+      Name = each.key
+    },
+    var.common_tags
+  )
+}
+
+module "Ansible" {
+  source        = "terraform-aws-modules/ec2-instance/aws"
+  ami           = data.aws_ami.devops_practice.id
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [local.security_group_id]
+  subnet_id              = local.public_subnet_ids[0]
+  user_data              = file("roboshop-ansible.sh")
+  
+  tags = merge(
+    {
+      Name = "Ansible"
     },
     var.common_tags
   )
