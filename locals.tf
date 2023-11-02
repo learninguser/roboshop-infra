f31c1e4fe9e8c4c4ba0d8b94e775a5f6d5984572
Pavan Kumar Akula <akulapavankumar@outlook.com>
2023-11-02 21:03:14 +0100

    Apply Auto indent

--- locals.tf
+++ locals.tf
@@ -1,9 +1,9 @@
 locals {
-  vpc_id = module.vpc.vpc_id
-  azs = module.vpc.azs
-  public_subnet_ids = module.vpc.public_subnet_ids
-  private_subnet_ids = module.vpc.private_subnet_ids
+  vpc_id              = module.vpc.vpc_id
+  azs                 = module.vpc.azs
+  public_subnet_ids   = module.vpc.public_subnet_ids
+  private_subnet_ids  = module.vpc.private_subnet_ids
   database_subnet_ids = module.vpc.database_subnet_ids
-  security_group_id = module.sg.sg_id
-  ip_address = module.ec2_instance
+  security_group_id   = module.sg.sg_id
+  ip_address          = module.ec2_instance
 }
